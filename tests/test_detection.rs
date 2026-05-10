1d55be3c1c5f1ce60aea8060820814163fdb9766
Sebastian Campos <sebash@mail.friendlyautomations.com>
2026-05-09 21:58:00 -0700

    detections working

--- tests/test_detection.rs
+++ tests/test_detection.rs
@@ -6,14 +6,19 @@ use video_cam::opencv_utils;
 #[tokio::test]
 async fn test_camera_detection() -> anyhow::Result<()>
 {
+    println!("{}", opencv::core::get_version_string()?);
     let mut camera = opencv_utils::open_camera()?;
     let mut frame = opencv_utils::get_frame(&mut camera)?;
     let mut writer = opencv_utils::open_writer("./test.avi", &frame)?;
-    let mut prediction_model = Yolo::new("../yolov8n_saved_model/yolov8n_float16.tflite", 256, 0.50, 0.50).unwrap();
+    let mut prediction_model = Yolo::new("yolov8n.onnx", 256, 0.50, 0.50).unwrap();
     for _ in 0..300 { // 10 seconds at 30 fps
         frame = opencv_utils::get_frame(&mut camera)?;
-        let detection = prediction_model.predict(frame)?;
-        print!("Class id: {}" , detection[0].class_id);
+        let detections = prediction_model.predict(frame)?;
+        if let Some(detection) = detections.first() {
+            println!("Detection: {}", detection.class_name);
+        } else {
+            println!("")
+        }
         sleep(Duration::from_millis(33)).await;
     }
     Ok(())
